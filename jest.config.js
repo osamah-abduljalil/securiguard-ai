70ad14e01a429cd27f7421c0cf3bb7d8fe14edd1
Osamah Abduljalil <50793839+osamah-abduljalil@users.noreply.github.com>
2025-05-09 20:04:06 +0300

    setup devops piplines

---
+++ jest.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  moduleNameMapper: {
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+    '\\.(gif|ttf|eot|svg|png)$': '<rootDir>/__mocks__/fileMock.js'
+  },
+  transform: {
+    '^.+\\.(js|jsx)$': 'babel-jest'
+  },
+  testMatch: ['**/__tests__/**/*.test.[jt]s?(x)'],
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coverageReporters: ['text', 'lcov'],
+  collectCoverageFrom: [
+    'src/**/*.{js,jsx}',
+    '!src/**/*.test.{js,jsx}',
+    '!src/index.js',
+    '!src/serviceWorker.js'
+  ]
+}; 
